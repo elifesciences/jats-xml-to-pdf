eb77158bc64966c49041f2246efb7af1a87d691d
Luke <lsh-0@users.noreply.github.com>
2018-10-26 18:25:52 +1030

    cassius is now producing something *finally*. still totally broken though

--- cassius/transform.sh
+++ cassius/transform.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-set -e
+set -ex
 input="$1"
 output="$2" # "out.pdf"
 output_name="${output%.*}" # "out"
 
-echo "got $input $output"
-
 cp "/mnt/$input" "/root/dtd/$input"
 
+# convert XML->HTML
 python ./CaSSius/cassius-import/bin/cassius-import.py "/root/dtd/$input" "/mnt/$output_name.html"
 
-wkhtmltopdf --javascript-delay 15000 --no-stop-slow-scripts -L 0 -R 0 -B 0 -T 0 "/mnt/$output_name.html" "/mnt/$output_name.pdf"
+# paths in the html reference the cassius dir
+(
+cd /mnt/
+ln -sf /root/CaSSius/cassius
+)
+
+# convert HTML->PDF
+wkhtmltopdf \
+    --javascript-delay 15000  \
+    --enable-external-links \
+    --no-stop-slow-scripts \
+    -L 0 -R 0 -B 0 -T 0 \
+    "/mnt/$output_name.html" "/mnt/$output_name.pdf"
