4f15f870d7b0e061c91a5cea0913ff731ebba153
Luke <lsh-0@users.noreply.github.com>
2018-10-26 18:27:03 +1030

    tweaks to run.sh

--- run.sh
+++ run.sh
@@ -6,11 +6,13 @@ transformers=( jats2latex cassius jats-xslt-stylesheets pandoc )
 
 mkdir -p "./pdf"
 
-for path in ./articles/*; do
-    fname=$(basename $path)
-    xml="./articles/$fname/elife-$fname-v1.xml"
+for zipfile in ./articles/*; do
+    fname=$(basename $zipfile) # 09560.zip
+    fname="${fname%.*}" # 09560
     for tname in "${transformers[@]}"; do
-        pdf="./pdf/$tname/$fname.pdf"
+        rm -rf "./$tname/mnt" # remove results of previous files
+        unzip $zipfile -d "./$tname/mnt/"
+        pdf="$tname--$fname.pdf" # cassius--09560.pdf
         ./$tname/run.sh $xml $pdf
     done
 done
