bd7002f9f043bca61ad0f7182c207a64483ffa23
Luke <lsh-0@users.noreply.github.com>
2018-10-29 17:25:31 +1030

    pandoc, xml->latex->pdf is more successful than xml->pdf

--- pandoc/transform.sh
+++ pandoc/transform.sh
@@ -2,5 +2,12 @@
 set -e
 input="$1"
 output="$2"
-echo "got $input and $output"
-pandoc -f jats "/mnt/$input" -o "/mnt/$output" --pdf-engine=xelatex
+
+# xml -> latex -> pdf
+# succeeds when direct xml -> pdf doesnt
+pandoc -f jats "/mnt/$input" -o "/mnt/$output.latex"
+pandoc -f latex "/mnt/$output.latex" -o "/mnt/$output" --pdf-engine=xelatex
+
+# xml -> pdf
+# errors in some cases
+#pandoc -f jats "/mnt/$input" -o "/mnt/$output" --pdf-engine=xelatex
