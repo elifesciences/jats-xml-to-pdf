7b0f07f5d9541620a5674aeb3161ce738194abf6
Luke <lsh-0@users.noreply.github.com>
2018-10-29 16:49:09 +1030

    jats-xslt-stylesheets, dtd spec is now downloaded from jats

--- jats-xslt-stylesheets/transform.sh
+++ jats-xslt-stylesheets/transform.sh
@@ -9,12 +9,13 @@ output="$2"
 # doesn't work, requires Saxon-PE+
 #java -jar /usr/share/java/saxon9he.jar -xsl:./JATSPreviewStylesheets-master/shells/saxon/jats-PMCcit-xslfo.xsl -s:"/mnt/$input" -o:"/mnt/$output"
 
+# jats dtd spec required in same dir as article files to generate fo file
+cp -R ./dtd/* /mnt/
+
 # generate the .fo file with saxon + jats stylesheet
 fofile="/mnt/out.fo"
-if [ ! -e "$fofile" ]; then
-    java -jar /usr/share/java/saxon9he.jar -xsl:./JATSPreviewStylesheets-master/xslt/main/jats-xslfo.xsl -s:"/mnt/$input" -o:out.fo
-    xmllint out.fo --format > "$fofile"
-fi
+java -jar /usr/share/java/saxon9he.jar -xsl:./JATSPreviewStylesheets-master/xslt/main/jats-xslfo.xsl -s:"/mnt/$input" -o:out.fo
+xmllint out.fo --format > "$fofile" # not really necessary, the fo file is unreadable
 
 # use Apache-FOP to generate pdf file
 ./fop-2.3/fop/fop -fo "$fofile" -pdf "/mnt/$output"
