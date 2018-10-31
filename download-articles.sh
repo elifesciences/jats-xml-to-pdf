a99e278ead7f99d57bdfebca364e9e33f5914643
Luke <lsh-0@users.noreply.github.com>
2018-10-31 17:43:00 +1030

    adds more articles, disables some others

--- download-articles.sh
+++ download-articles.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -eu
 
-article_list=( 09560 12059 19285 20317 31543 )
+# 09560 -- homo naledi
+# -- article is too large. 12059 -- random
+# 19258 -- random
+# 20317 -- random
+# 31543 -- random
+# 00569 -- fringe, "Everything!"
+# 05378 -- fringe, "Grey rows in Table"
+# -- article is too large. 18566 -- fringe, "Appendix with 67 tables and 243 equations"
+
+article_list=( 09560 19285 20317 31543 00569 05378 18566 )
 (
     cd articles
     for article in "${article_list[@]}"; do
