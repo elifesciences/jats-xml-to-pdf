b7175f690576a700a3ff6cc0c8f9cbfe03352655
Luke <lsh-0@users.noreply.github.com>
2018-11-06 17:23:12 +1030

    pandoc reverts to a simpler transformation when initial attempt fails this simpler transformation may be missing information

--- pandoc/transform.sh
+++ pandoc/transform.sh
@@ -4,17 +4,25 @@ input="$1"
 output="$2" # "out.pdf"
 output_name="${output%.*}" # "out"
 
-# to compare to input xml
-# pandoc -f jats "/mnt/$input" -t jats -o "/mnt/$output_name.xml"
-
 # this is the intermediate tex file before becoming PDF
 # the -s or --standalone flag is necessary else it strips header and title information?
-pandoc -f jats "/mnt/$input" -s -o "/mnt/$output_name-intermediate.tex" -t latex --filter ./abstract.sh
+pandoc -f jats "/mnt/$input" --standalone -o "/mnt/$output_name-intermediate.tex" -t latex #--filter ./abstract.sh
 
 # xml -> latex -> pdf, using custom intermediate template
 # often succeeds when direct xml->pdf doesnt
 pandoc \
     -f latex "/mnt/$output_name-intermediate.tex" \
     -o "/mnt/$output_name.pdf" \
-    --template=/root/latex-template.tex \
-    --pdf-engine=xelatex
+    --pdf-engine=xelatex \
+    --template=/root/latex-template.tex || {
+
+        echo "failed, attempting non-standalone conversion"
+
+        # try rendering using a non-standalone template. the output will be incomplese but you'll have *something*
+        pandoc -f jats "/mnt/$input" -o "/mnt/$output_name-intermediate2.tex" -t latex #--filter ./abstract.sh
+        pandoc \
+            -f latex "/mnt/$output_name-intermediate2.tex" \
+            -o "/mnt/$output_name.pdf" \
+            --pdf-engine=xelatex \
+            --template=/root/latex-template.tex
+}
