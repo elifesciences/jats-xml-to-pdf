9b944fa451d5c39c99ace2a7f09a7dee86a3767b
Luke <lsh-0@users.noreply.github.com>
2018-11-06 11:56:53 +1030

    pandoc now uses a custom template to generate pdf

--- pandoc/transform.sh
+++ pandoc/transform.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 set -e
 input="$1"
-output="$2"
+output="$2" # "out.pdf"
+output_name="${output%.*}" # "out"
 
-# xml -> latex -> pdf
-# succeeds when direct xml -> pdf doesnt
-pandoc -f jats "/mnt/$input" -o "/mnt/$output.latex"
-pandoc -f latex "/mnt/$output.latex" -o "/mnt/$output" --pdf-engine=xelatex
+# to compare to input xml
+# pandoc -f jats "/mnt/$input" -t jats -o "/mnt/$output_name.xml"
 
-# xml -> pdf
+# this is the intermediate tex file before becoming PDF
+# the -s or --standalone flag is necessary else it strips header and title information?
+pandoc -f jats "/mnt/$input" -s -o "/mnt/$output_name-intermediate.tex" -t latex
+
+# xml -> pdf using custom intermediate template
 # errors in some cases
-#pandoc -f jats "/mnt/$input" -o "/mnt/$output" --pdf-engine=xelatex
+pandoc \
+    -f jats "/mnt/$input" \
+    -o "/mnt/$output_name.pdf" \
+    --template=/root/latex-template.tex \
+    --pdf-engine=xelatex || {
+
+        # TODO: scrap this in favour of using the standalone intermediate directly
+        echo "direct jats->pdf failed, attempting jats->latex->pdf"
+        # xml -> latex -> pdf
+        # succeeds when direct xml -> pdf doesnt
+        # this is *more* lossy
+        pandoc -f jats "/mnt/$input" -o "/mnt/$output_name.latex"
+        pandoc -f latex "/mnt/$output_name.latex" -o "/mnt/$output_name.pdf" --pdf-engine=xelatex
+}
